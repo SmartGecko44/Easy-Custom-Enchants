6bc0119ab475d9f97d76bb5504373cbaf86c822f
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2025-02-15 13:39:24 +0100

    Initial commit

--- src/main/kotlin/me/gecko/easyCustomEnchants/EasyCustomEnchants.kt
+++ src/main/kotlin/me/gecko/easyCustomEnchants/EasyCustomEnchants.kt
@@ -1,11 +1,14 @@
 package me.gecko.easyCustomEnchants
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class EasyCustomEnchants : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        Bukkit.getConsoleSender().sendMessage("Initializing EasyCustomEnchants")
+
     }
 
     override fun onDisable() {
