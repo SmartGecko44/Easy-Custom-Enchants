714225ee2228b461382dc1755e11a862e06ee7fc
Gecko! <74975165+SmartGecko44@users.noreply.github.com>
2025-02-15 13:39:24 +0100

    Initial commit

---
+++ src/main/kotlin/me/gecko/easyCustomEnchants/EasyCustomEnchants.kt
@@ -0,0 +1,14 @@
+package me.gecko.easyCustomEnchants
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EasyCustomEnchants : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
